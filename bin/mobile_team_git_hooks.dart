0d4c29737478006bae49ee97ec480cb852a6923b
Nguyen, Phuoc <phuoc.nguyen2@hrs.com>
2025-02-05 10:47:31 +0700

    mobile teams git hooks

---
+++ bin/mobile_team_git_hooks.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_team_git_hooks/mobile_team_git_hooks.dart' as mobile_team_git_hooks;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_team_git_hooks.calculate()}!');
+}
---
+++ test/mobile_team_git_hooks_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_team_git_hooks/mobile_team_git_hooks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
