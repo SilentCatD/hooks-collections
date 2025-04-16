93f47e58efa2b98a1bf137a1136a067f7b9eba39
Silent Cat <ngocphuocnguyen312@gmail.com>
2025-04-16 19:05:08 +0700

    license checker

--- src/flutter_hook.py
+++ src/flutter_hook.py
@@ -5,8 +5,23 @@ import subprocess
 
 class FlutterHook:
     """helper for dart flutter"""
+
     def __init__(self, files: list[str]):
         self.files = grep_files_by_extension(files, 'dart')
+        self.pubspec_file_changed = 'pubspec.lock' in files
+
+    def license_check(self) -> bool:
+        if not self.pubspec_file_changed:
+            return True
+        installation_args = ["dart", "pub", "global", "activate", "license_checker"]
+        subprocess.run(installation_args)
+        scanning_results = ["lic_ck", "check-licenses", "--config"]
+        config_file_name = 'license_checker.yaml'
+        scanning_results.append(config_file_name)
+        license_scan_result = subprocess.run(installation_args)
+        if license_scan_result.returncode != 0:
+            return False
+        return True
 
     def format_files(self) -> bool:
         """format dart files"""
